ca227df25e5c561d0a7d157ff5d8f66e6a29095d
green <greenn.lab@gmail.com>
2022-06-06 07:57:54 +0900

    Initial commit

---
+++ src/main/java/com/example/tastecheckstyle/some/SomeController.java
@@ -0,0 +1,2 @@
+package com.example.tastecheckstyle.some;public class SomeController {
+}
---
+++ src/test/java/com/example/tastecheckstyle/TasteCheckstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tastecheckstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteCheckstyleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
